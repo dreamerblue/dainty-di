987c97a3b73b7f40412c3458519a161d99b6c8e1
bLue <tbdblue@gmail.com>
2023-04-10 19:51:25 +0800

    chore: modify post-build options

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@ rm -rf dist
 tsc
 files=$(find dist/*.js)
 while read -r line; do
-  esbuild $line --minify --allow-overwrite --outfile=$line --platform=node
+  esbuild $line --outfile=$line --allow-overwrite --minify --platform=node --format=cjs --target=es2015
 done <<< "$files"
