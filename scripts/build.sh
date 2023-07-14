b11abcf0439a961ae21c6fe1c993884c95703ff3
bLue <tbdblue@gmail.com>
2023-07-14 10:38:44 +0800

    release: 1.0.0

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-rm -rf dist
-tsc
-files=$(find dist/*.js)
-while read -r line; do
-  esbuild $line --outfile=$line --allow-overwrite --minify --platform=node --format=cjs --target=es2015
-done <<< "$files"
